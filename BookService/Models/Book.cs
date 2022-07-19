6ea77143506871faa90e37e7e0966211987f3096
gauravc574 <gauravchawre580@gmail.com>
2022-07-19 13:32:48 +0530

    BookStore Web Api project added

---
+++ BookService/Models/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookService.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string  Title { get; set; }
+        public int Year { get; set; }
+        public decimal Price { get; set; }
+        public string Genre { get; set; }
+
+
+        //Foreign Key
+        
+        public int AuthorId { get; set; }
+
+        //Navigation property
+        //assuming that each book has a single author.
+        public Author Author { get; set; }
+    }
+}
---
+++ WebAPIDemo/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPIDemo.Models
+{
+    public class Employee
+    {
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public double Salary { get; set; }
+        public string Department { get; set; }
+         
+    }
+}
